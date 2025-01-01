edab0cdd0c84451fd068e1ac5f31be12fa5f49ed
Logan Minaudo <71421100+lminaudo@users.noreply.github.com>
2025-01-01 12:53:20 -0500

    Update to file structure.

--- componets/script.js
+++ componets/script.js
@@ -15,6 +15,6 @@ function loadContent(targetSection, filePath){
         })
 }
 document.addEventListener("DOMContentLoaded", ()=>{
-    loadContent("header", "/componets/header.html")
-    loadContent("head", "/componets/head.html")
+    loadContent("header", "componets/header.html")
+    loadContent("head", "componets/head.html")
 })
