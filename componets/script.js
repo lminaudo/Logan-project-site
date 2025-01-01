3e26e6e0312fb8435a0da5dd3dcb50f6524a6dd3
Logan Minaudo <71421100+lminaudo@users.noreply.github.com>
2025-01-01 11:46:16 -0500

    Changed file structure and updates some of the files.

---
+++ componets/script.js
@@ -0,0 +1,19 @@
+function loadContent(targetSection, filePath){
+    fetch(filePath)
+        .then((response)=>{
+            if(!response.ok){
+                throw new Error(`Could not fetch ${filePath}: ${response.statusText}`);
+
+            }
+            return response.text();
+        })
+        .then((data) =>{
+            document.querySelector(targetSection).innerHTML = data;
+        })
+        .catch((error)=>{
+            console.error(error);
+        })
+}
+document.addEventListener("DOMContentLoaded", ()=>{
+    loadContent("header", "../componets/header.html")
+})
