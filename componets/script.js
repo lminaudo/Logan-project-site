605bac152fb205c61aa4eece67507a2a6ea7b1a9
Logan Minaudo <71421100+lminaudo@users.noreply.github.com>
2025-01-01 18:35:00 -0500

    Add animations

--- componets/script.js
+++ componets/script.js
@@ -17,3 +17,17 @@ function loadContent(targetSection, filePath){
 document.addEventListener("DOMContentLoaded", ()=>{
     loadContent("header", "componets/header.html")
 })
+
+const animation = document.querySelectorAll('.experience');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            animation.classList.add('experience');
+            return;
+        }
+        animation.classList.remove('experience');
+    });
+});
+animation.forEach((element)=>observer.observe(element));
+// observer.observe(document.querySelector('.experience-wrapper'));
